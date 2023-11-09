3fc4c3973a3e3546de266062db35ca268e67fe55
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-11-09 21:28:36 +0300

    добавлен скрипт для баллов

---
+++ Assets/Scripts/MarksCount.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarksCount : MonoBehaviour
+{
+    [SerializeField] private float[] marks;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
