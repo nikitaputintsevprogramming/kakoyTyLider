0d28b7f0ef75009a74ff67a454bfc425f5bd69cb
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-11-10 14:25:51 +0300

    готовы

--- Assets/Scripts/MarksCount.cs
+++ Assets/Scripts/MarksCount.cs
@@ -16,6 +16,8 @@ namespace Marks
 
         public Image[] _resultDiagramms;
         [SerializeField] private float _speedDiagram = 2f;
+        [SerializeField] private float _pos100 = 220f;
+        [SerializeField] private float _size100 = 440f;
 
         [SerializeField] private bool isOnResults;
 
@@ -48,12 +50,36 @@ namespace Marks
         }
 
         [ContextMenu("MarksResultOn")]
+        //public void MarksResultOn()
+        //{
+        //    _pos100 = 100 / _MarksInBlocks[0];
+        //    _size100 = _pos100 * 2f;
+        //    _resultDiagramms[0].rectTransform.anchoredPosition = Vector2.Lerp(_resultDiagramms[0].rectTransform.anchoredPosition, new Vector2(_resultDiagramms[0].rectTransform.anchoredPosition.x, _pos100), _speedDiagram * Time.deltaTime);
+        //    _resultDiagramms[0].rectTransform.sizeDelta = Vector2.Lerp(_resultDiagramms[0].rectTransform.sizeDelta,
+        //                    new Vector2(_resultDiagramms[0].rectTransform.sizeDelta.x,
+        //                    _size100), _speedDiagram * Time.deltaTime);
+              
+        //}
         public void MarksResultOn()
         {
-            _resultDiagramms[0].rectTransform.anchoredPosition = Vector2.Lerp(_resultDiagramms[0].rectTransform.anchoredPosition, new Vector2(_resultDiagramms[0].rectTransform.anchoredPosition.x, 225f), _speedDiagram * Time.deltaTime);
-            _resultDiagramms[0].rectTransform.sizeDelta = Vector2.Lerp(_resultDiagramms[0].rectTransform.sizeDelta,
-                            new Vector2(_resultDiagramms[0].rectTransform.sizeDelta.x,
-                            450f), _speedDiagram * Time.deltaTime);
+            for(int count = 0; count < _MarksInBlocks.Length; count++)
+            {
+                float result = _MarksInBlocks[count] / 3.0f;
+                int roundedResult = Mathf.RoundToInt(result * 100); // Умножаем на 100, чтобы перевести дробное число в проценты и округляем
+
+                //Debug.Log(roundedResult);
+                _resultDiagramms[count].GetComponentInChildren<Text>().text = roundedResult.ToString();
+                if (result >= 1.6f) // Если результат положительный
+                {
+                    _resultDiagramms[count].rectTransform.sizeDelta = Vector2.Lerp(_resultDiagramms[count].rectTransform.sizeDelta, new Vector2(_resultDiagramms[count].rectTransform.sizeDelta.x, _size100), _speedDiagram * Time.deltaTime); // Установка размера блока на 100 процентов
+                    _resultDiagramms[count].rectTransform.anchoredPosition = Vector2.Lerp(_resultDiagramms[count].rectTransform.anchoredPosition, new Vector2(_resultDiagramms[count].rectTransform.anchoredPosition.x, _pos100), _speedDiagram * Time.deltaTime); // Установка позиции блока на 100 процентов
+                }
+                else // Если результат требует доработки
+                {
+                    _resultDiagramms[count].rectTransform.sizeDelta = Vector2.Lerp(_resultDiagramms[count].rectTransform.sizeDelta, new Vector2(_resultDiagramms[count].rectTransform.sizeDelta.x, _size100 * result), _speedDiagram * Time.deltaTime); // Установка размера блока в соответствии с результатом
+                    _resultDiagramms[count].rectTransform.anchoredPosition = Vector2.Lerp(_resultDiagramms[count].rectTransform.anchoredPosition, new Vector2(_resultDiagramms[count].rectTransform.anchoredPosition.x, _pos100 * result), _speedDiagram * Time.deltaTime); // Установка позиции блока в соответствии с результатом
+                }
+            }
         }
 
         public void ShowRes()
