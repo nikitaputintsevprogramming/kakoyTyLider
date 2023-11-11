5159a630b5d3708ca943a3190c91fffc6a9b7c6c
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-11-11 14:08:59 +0300

    osk

---
+++ Assets/OSK/Assets/Scripts/KeyboardScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardScript : MonoBehaviour
+{
+
+    public InputField TextField;
+    [SerializeField] private InputField[] TextFields;
+    public GameObject RusLayoutSml, RusLayoutBig, EngLayoutSml, EngLayoutBig, SymbLayout;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SelectField();
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                SelectField();
+            }
+        }
+    }
+
+    private void SelectField()
+    {
+        for(int count = 0; count <= TextFields.Length; count++)
+        {
+            if (TextFields[count].isFocused)
+                TextField = TextFields[count];
+        }
+    }
+
+    public void alphabetFunction(string alphabet)
+    {
+
+
+        TextField.text=TextField.text + alphabet;
+
+    }
+
+    public void BackSpace()
+    {
+
+        if(TextField.text.Length>0) TextField.text= TextField.text.Remove(TextField.text.Length-1);
+
+    }
+
+    public void CloseAllLayouts()
+    {
+
+        RusLayoutSml.SetActive(false);
+        RusLayoutBig.SetActive(false);
+        EngLayoutSml.SetActive(false);
+        EngLayoutBig.SetActive(false);
+        SymbLayout.SetActive(false);
+
+    }
+
+    public void ShowLayout(GameObject SetLayout)
+    {
+
+        CloseAllLayouts();
+        SetLayout.SetActive(true);
+
+    }
+
+}
---
+++ Assets/Scripts/InputFieldManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldManager : MonoBehaviour
+{
+    [SerializeField] private InputField _inputField;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Debug.Log(_inputField.isFocused);
+    }
+}
