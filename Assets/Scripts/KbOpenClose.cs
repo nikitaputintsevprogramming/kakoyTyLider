d8d6df89ac3b42c456dfa27be2670fbca80f9e6b
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-11-15 01:49:39 +0300

    изм

--- Assets/Scripts/KbOpenClose.cs
+++ Assets/Scripts/KbOpenClose.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class KbOpenClose : MonoBehaviour
+public class KbOpenClose : MonoBehaviour, ISelectHandler
 {
     [SerializeField] private GameObject OSK;
 
     public void OpenClose()
     {
         OSK.SetActive(!OSK.activeSelf);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        OSK.SetActive(true);
     }
 }
